2fdb0611d1c0ca6a8ad308a0ec902c02da2919f5
master <junu5810@naver.com>
2025-09-29 10:56:07 +0900

    주식데이터

--- database_stock.py
+++ database_stock.py
@@ -20,12 +20,19 @@ info = soup.select_one("#_nowVal")
 
 price = info.text
 
+# MYSQL 같은모듈, 파이썬에 기본 내장된 데이터베이스시스템
 import sqlite3
 
+# 전제: 이 파이썬 파일과 같은 폴더 내에 .db 파일이 하나 존재한다!
+
+# 이 파이썬 파일과 데이터베이스 파일을 연결하여 작업 가능하게 만든다!
 con= sqlite3.connect("database_stock.db")
 
+# 연결이 되었다면, 연결된 파일에 sql 구문을 전달해서 실행할 객체를 생성해야한다!
 cur= con.cursor()
 
+# 테이블 생성 명령 실행
+# cur.execute("CREATE TABLE customer(name TEXT, age INT)") #테이블이 생성되면 더이상 작동하지않는다
 cur.execute("INSERT INTO stocks VALUES(?,?)", ('한화투자증권', price))
 
 con.commit()
